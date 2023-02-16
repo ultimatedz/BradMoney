133f9e9a7fa19e19ed752bf8cde68fad733c1798
Luis Fernando da Silva <s.fernando13@pucpr.edu.br>
2023-02-16 17:02:26 -0300

    Created slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatCardModule} from '@angular/material/card';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  count = 1
 
+  ngOnInit(): void {
+    const radioElement: HTMLInputElement = document.querySelector('#customer1')!
+    if(radioElement){
+      radioElement.checked = true
+    }
+
+    setInterval(() => {
+      this.nextImage()
+    }, 3000)
+  }
+
+  nextImage(){
+    this.count++
+    if(this.count>4){
+      this.count = 1
+    }
+
+    const elementSelected: HTMLInputElement = document.querySelector(`#customer${this.count}`)!
+    elementSelected.checked = true
+  }
 }
