2baaeba343fcef36ffdbb2ee7602a2389756808f
Caio Henrique <caio_m_meira@hotmail.com>
2023-02-18 06:42:10 -0300

    basic routing and html template for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { NewPasswordComponent } from './pages/new-password/new-password.component';
+import {DashboardHomeComponent} from './features/dashboard/pages/dashboard-home/dashboard-home.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -13,7 +13,8 @@ const routes: Routes = [
   {path: 'recovery', component: RecoveryComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'newPassword', component: NewPasswordComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardHomeComponent}
+  
 ];
 
 @NgModule({
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { DashboardHomeComponent } from './dashboard/pages/dashboard-home/dashboard-home.component';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardHomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class FeaturesModule { }
